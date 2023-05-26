25f74516e8bec093e5c19c176ab2d68138b0be7a
BanditCook <tanguy.lalexandre@gmail.com>
2023-05-26 11:32:27 +0100

    Pause of the project.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,22 +1,49 @@
-const myPokemonName = document.querySelector(".name");
-const myPokemonImg = document.querySelector(".pokemonImg");
+const pokemonName = document.querySelector(".name");
+const pokemonImg = document.querySelector(".pokemonImg");
 let randomPokemon = Math.floor(Math.random() * 151 + 1);
-let pokemonNumber = 7;
-async function getPokemon() {
-  await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`)
-    .then((res) => res.json())
-    .then((data) => {
-      generatePokemon(data);
+let pokemonNumber = 2;
 
-      console.log(data);
-    });
+class Pokemon {
+  constructor(data, id, name, img, ability1, ability2) {
+    this.id = data.id;
+    this.name = data.name;
+    this.img = data.img;
+    this.ability1 = data.ability1;
+    this.ability2 = data.ability2;
+  }
 }
-
+async function getPokemon() {
+  try {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`
+    );
+    if (response.ok) {
+      const data = await response.json();
+      const id = data.id;
+      const name = data.name;
+      const img = data.sprites.front_default;
+      const ability1 = data.abilities[0].ability.name;
+      const ability2 = data.abilities[1].ability.name;
+    } else {
+      throw new Error("something went wrong");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
 getPokemon();
-const generatePokemon = (data) => {
-  const result = data;
-  myPokemonName.innerHTML = result.name;
-  myPokemonImg.src = result.sprites.back_default;
-  ability1.innerHTML = result.abilities[0].ability.name;
-  ability2.innerHTML = result.abilities[1].ability.name;
-};
+
+// let pokemon = new Pokemon(
+
+// let mine = document.querySelector("#mine");
+
+// let bulbasaur = new Pokemon(
+//   data,
+//   1,
+//   data.name,
+//   data.img,
+//   data.abilities[0].ability.name,
+//   data.abilities[1].ability.name
+// );
+
+// console.log(bulbasaur);
